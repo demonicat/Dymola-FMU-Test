6deb0e6da2377f30732e6f0f5759feec2b836e59
DESKTOP-PCD8921\mechm <mech.mancer@gmail.com>
2021-03-10 13:48:34 -0800

    Enabled Basic Controller Movement Interaction

---
+++ Assets/Drone/DroneController.cs
@@ -0,0 +1,240 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/DroneController.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @DroneController : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @DroneController()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""DroneController"",
+    ""maps"": [
+        {
+            ""name"": ""Gameplay"",
+            ""id"": ""8a4c461d-2d10-43db-9727-e195c313db99"",
+            ""actions"": [
+                {
+                    ""name"": ""X_Neg"",
+                    ""type"": ""Value"",
+                    ""id"": ""fa2d4588-f645-4a85-9f93-3a632deade5b"",
+                    ""expectedControlType"": ""Axis"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""X_Pos"",
+                    ""type"": ""Value"",
+                    ""id"": ""a26d5dd4-1bd5-4e0c-b581-eeb9f00cb8d0"",
+                    ""expectedControlType"": ""Axis"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Y_Neg"",
+                    ""type"": ""Value"",
+                    ""id"": ""cac0dad2-5f8a-4e60-a667-8c60dd9285a7"",
+                    ""expectedControlType"": ""Axis"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Y_Pos"",
+                    ""type"": ""Value"",
+                    ""id"": ""05b16ab4-e6eb-408d-aee8-563667c4c141"",
+                    ""expectedControlType"": ""Axis"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""5a176517-9214-48d2-88f8-4e9931713e15"",
+                    ""path"": ""<Gamepad>/leftStick/left"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""General Gamepad"",
+                    ""action"": ""X_Neg"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""dad3d6c5-25e3-42a0-9de4-825f8a38d591"",
+                    ""path"": ""<Gamepad>/leftStick/right"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""General Gamepad"",
+                    ""action"": ""X_Pos"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""1e3490f3-c624-4ee9-9c99-360401358d24"",
+                    ""path"": ""<Gamepad>/leftStick/down"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""General Gamepad"",
+                    ""action"": ""Y_Neg"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""ac25abba-7b82-4dab-b87c-71a013502223"",
+                    ""path"": ""<Gamepad>/leftStick/up"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""General Gamepad"",
+                    ""action"": ""Y_Pos"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""General Gamepad"",
+            ""bindingGroup"": ""General Gamepad"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Gamepad>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+        // Gameplay
+        m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
+        m_Gameplay_X_Neg = m_Gameplay.FindAction("X_Neg", throwIfNotFound: true);
+        m_Gameplay_X_Pos = m_Gameplay.FindAction("X_Pos", throwIfNotFound: true);
+        m_Gameplay_Y_Neg = m_Gameplay.FindAction("Y_Neg", throwIfNotFound: true);
+        m_Gameplay_Y_Pos = m_Gameplay.FindAction("Y_Pos", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Gameplay
+    private readonly InputActionMap m_Gameplay;
+    private IGameplayActions m_GameplayActionsCallbackInterface;
+    private readonly InputAction m_Gameplay_X_Neg;
+    private readonly InputAction m_Gameplay_X_Pos;
+    private readonly InputAction m_Gameplay_Y_Neg;
+    private readonly InputAction m_Gameplay_Y_Pos;
+    public struct GameplayActions
+    {
+        private @DroneController m_Wrapper;
+        public GameplayActions(@DroneController wrapper) { m_Wrapper = wrapper; }
+        public InputAction @X_Neg => m_Wrapper.m_Gameplay_X_Neg;
+        public InputAction @X_Pos => m_Wrapper.m_Gameplay_X_Pos;
+        public InputAction @Y_Neg => m_Wrapper.m_Gameplay_Y_Neg;
+        public InputAction @Y_Pos => m_Wrapper.m_Gameplay_Y_Pos;
+        public InputActionMap Get() { return m_Wrapper.m_Gameplay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GameplayActions set) { return set.Get(); }
+        public void SetCallbacks(IGameplayActions instance)
+        {
+            if (m_Wrapper.m_GameplayActionsCallbackInterface != null)
+            {
+                @X_Neg.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnX_Neg;
+                @X_Neg.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnX_Neg;
+                @X_Neg.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnX_Neg;
+                @X_Pos.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnX_Pos;
+                @X_Pos.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnX_Pos;
+                @X_Pos.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnX_Pos;
+                @Y_Neg.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnY_Neg;
+                @Y_Neg.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnY_Neg;
+                @Y_Neg.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnY_Neg;
+                @Y_Pos.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnY_Pos;
+                @Y_Pos.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnY_Pos;
+                @Y_Pos.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnY_Pos;
+            }
+            m_Wrapper.m_GameplayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @X_Neg.started += instance.OnX_Neg;
+                @X_Neg.performed += instance.OnX_Neg;
+                @X_Neg.canceled += instance.OnX_Neg;
+                @X_Pos.started += instance.OnX_Pos;
+                @X_Pos.performed += instance.OnX_Pos;
+                @X_Pos.canceled += instance.OnX_Pos;
+                @Y_Neg.started += instance.OnY_Neg;
+                @Y_Neg.performed += instance.OnY_Neg;
+                @Y_Neg.canceled += instance.OnY_Neg;
+                @Y_Pos.started += instance.OnY_Pos;
+                @Y_Pos.performed += instance.OnY_Pos;
+                @Y_Pos.canceled += instance.OnY_Pos;
+            }
+        }
+    }
+    public GameplayActions @Gameplay => new GameplayActions(this);
+    private int m_GeneralGamepadSchemeIndex = -1;
+    public InputControlScheme GeneralGamepadScheme
+    {
+        get
+        {
+            if (m_GeneralGamepadSchemeIndex == -1) m_GeneralGamepadSchemeIndex = asset.FindControlSchemeIndex("General Gamepad");
+            return asset.controlSchemes[m_GeneralGamepadSchemeIndex];
+        }
+    }
+    public interface IGameplayActions
+    {
+        void OnX_Neg(InputAction.CallbackContext context);
+        void OnX_Pos(InputAction.CallbackContext context);
+        void OnY_Neg(InputAction.CallbackContext context);
+        void OnY_Pos(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Drone/DroneMovement.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMI2;
+using UnityEngine.InputSystem; 
+
+//Basic test of FMU input and output visualized with Unity.
+public class DroneMovement : MonoBehaviour
+{
+    DroneController inputActions;
+    FMU fmu;
+    float xcoord, ycoord, zcoord = 0; 
+    float xdir, ydir, zdir; 
+
+    void Awake()
+    {
+        Application.targetFrameRate = 300; 
+
+        inputActions = new DroneController();
+        OnEnable();
+        inputActions.Gameplay.X_Neg.performed += ctx => decreaseX();
+        inputActions.Gameplay.X_Pos.performed += ctx => increaseX();
+        inputActions.Gameplay.Y_Neg.performed += ctx => decreaseY();
+        inputActions.Gameplay.Y_Pos.performed += ctx => increaseY();
+
+        //Instantiate the FMU
+        fmu = new FMU("DroneSimulation_IdealMachine", name);
+        fmu.Reset();
+        fmu.SetupExperiment(Time.time);
+        fmu.EnterInitializationMode();
+        fmu.ExitInitializationMode();
+
+    }
+
+    void FixedUpdate()
+    {
+        print("X: " + xcoord + " Y: " +  ycoord + " Z: " + zcoord);
+        setCoord(); 
+
+        //Synchronize the model with the current time
+        fmu.DoStep(Time.time, Time.deltaTime);
+        //Read FMU outputs of position 
+        xdir = (float)fmu.GetReal("xgps");
+        ydir = (float)fmu.GetReal("ygps");
+        zdir = (float)fmu.GetReal("zgps");
+        //Assign gps values to transform of object
+        transform.position = new Vector3(xdir, ydir, zdir);
+    }
+
+    //Function to pass through ideal coordinates
+    void setCoord()
+    { 
+        fmu.SetReal("xcoord", xcoord);
+        fmu.SetReal("zcoord", ycoord);
+        fmu.SetReal("ycoord", zcoord);
+    }
+
+    void decreaseX()
+    {
+        xcoord -= (float)0.01;
+    }
+
+    void increaseX()
+    {
+        xcoord += (float)0.01;
+    }
+
+    void decreaseY()
+    {
+        ycoord -= (float)0.01;
+    }
+
+    void increaseY()
+    {
+        ycoord += (float)0.01;
+    }
+
+    void OnEnable()
+    {
+        inputActions.Gameplay.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Gameplay.Disable();
+    }
+    //General clean up
+    void OnDestroy()
+    {
+        fmu.FreeInstance();
+        fmu.Dispose();
+    }
+
+}
